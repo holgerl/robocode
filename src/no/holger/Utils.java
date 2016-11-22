bae1638798a4dafad58bf9be22eb789ede86338c
Holger Ludvigsen <holger.ludvigsen@bekk.no>
2016-11-22 18:47:17 +0100

    Bugfix in clamp (which was apparently not used)

--- src/no/holger/Utils.java
+++ src/no/holger/Utils.java
@@ -2,6 +2,6 @@ package no.holger;
 
 public class Utils {
     public static Double clamp(Double x, Double min, Double max) {
-        return x < min ? (x > max ? max : x) : min;
+        return x > min ? (x < max ? x : max) : min;
     }
 }
