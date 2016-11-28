ba619226c3ccf3290ae7dcc023471f05013b4ac5
Holger Ludvigsen <ludvigsen@gmail.com>
2016-11-28 19:05:49 +0100

    Added a target practice bot

---
+++ src/no/holger/TargetPractice.java
@@ -0,0 +1,35 @@
+package no.holger;
+
+import robocode.AdvancedRobot;
+import robocode.HitRobotEvent;
+import robocode.Rules;
+import robocode.ScannedRobotEvent;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static no.holger.Utils.clamp;
+
+public class TargetPractice extends AdvancedRobot {
+
+    public void run() {
+        Colors.applyColors(this);
+
+        setAdjustGunForRobotTurn(true);
+        setAdjustRadarForGunTurn(true);
+        setAdjustRadarForRobotTurn(true);
+
+        while (true) {
+            moveBot();
+            execute();
+        }
+    }
+
+    private void moveBot() {
+        setAhead(500);
+
+        setTurnRightRadians(500);
+    }
+}
